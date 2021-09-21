c24716d62551e4e505a7311f2ed764a03b79d487
Kara <kara_himanshu@yahoo.com>
2021-09-21 17:30:22 +0530

    Added Components Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Exhanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+  Homepage,
+} from "./components";
 import "./App.css";
 
 const App = () => {
@@ -10,7 +17,29 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main">Main</div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exhanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer">Footer</div>
     </div>
   );

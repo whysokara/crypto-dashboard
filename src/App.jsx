4c0dc3b4f76af9fccb451b5fa61e01fbc198826b
Kara <kara_himanshu@yahoo.com>
2021-09-21 12:36:36 +0530

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse</h1>{" "}
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">Main</div>
+      <div className="footer">Footer</div>
     </div>
   );
 };
